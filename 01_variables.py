2b18db938eba91bb4045639147d9ea50787840e0
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-07-27 17:59:45 +0500

    variables

--- 01_variables.py
+++ 01_variables.py
@@ -20,3 +20,11 @@ def id_f(user, uid):
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+'''
+    trailing slash/
+    Both the rules appear similar but in the second rule, trailing slash (/) is used.
+    As a result, it becomes a canonical URL. 
+    Hence, using /python or /python/ returns the same output. 
+    However, in case of the first rule, /flask/ URL results in 404 Not Found page.
+'''
