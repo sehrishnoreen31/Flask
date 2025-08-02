7f7c05f991ca90d15538b6885c0c014b2c31b825
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-08-02 18:21:24 +0500

    post stores endpoint

--- storeapp/app.py
+++ storeapp/app.py
@@ -18,7 +18,18 @@ stores = [
 def get_stores():
     return {'stores': stores}
 
-
+# post request
+@app.post('/stores/')
+def create_stores():
+    request_data = request.get_json()
+    store_names = [store['name'] for store in stores]
+    if request_data['name'] in store_names:
+        return {'message': 'store already exists'}, 409
+    else:
+        new_store = {'name': request_data['name']}
+        stores.append(new_store)
+        return new_store, 201
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
